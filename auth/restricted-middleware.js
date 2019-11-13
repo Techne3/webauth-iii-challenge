8468dc93861eea1a77333de0d6eb6b7f9b1d9cf4
Timothy Sharkey <Techne3@gmail.com>
2019-11-13 13:45:47 -0700

    Set up JWT authorization for users

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,27 @@
+
+const jwt = require('jsonwebtoken') // 1 npm i jsonwebtoken
+const secret = process.env.JWT_SECRET || 'is it secret, is it safe?'
+
+module.exports = (req, res, next) => {
+//   const { username, password } = req.headers;
+
+  const token = req.headers.authorization
+
+  if (token) {
+    //check that the token is valid
+    jwt.verify(token,secret, (err, decodedToken)=> {
+      if(err){
+        // bad panda, token has been tampered with
+        res.status(401).json({message: 'invalid credentials'})
+      }else{
+         req.decodedJwt = decodedToken
+       next();
+      }
+    })
+
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+};
+
+
